58a9cb027bb26e829f68faa044c71cf7c4c3e980
minjaeleee <dlalswo4602@naver.com>
2023-02-17 15:32:00 +0900

    private class 제거, font 변경기능 추가

--- src/js/keyboard.js
+++ src/js/keyboard.js
@@ -1,18 +1,21 @@
 export class Keyboard {
-  #switchEl;
   constructor() {
-    this.#assignElement()
-    this.#addEvent()
+    this.assignElement()
+    this.addEvent()
   }
-  #assignElement() {
-    this.#switchEl = document.getElementById('switch')
+  assignElement() {
+    this.switchEl = document.getElementById('switch');
+    this.fontSelectEl = document.getElementById('font');
   }
-  #addEvent() {
-    this.#switchEl = document.addEventListener('change', event => {
+  addEvent() {
+    this.switchEl = document.addEventListener('change', event => {
       document.documentElement.setAttribute(
         'theme',
-        event.target.checked ? 'dark-mode' : ''
+        event.target.checked ? 'dark-mode' : 'white-mode'
       )
+    })
+    this.fontSelectEl = document.addEventListener('change', event => {
+      document.body.style.fontFamily = event.target.value
     })
   }
 }
