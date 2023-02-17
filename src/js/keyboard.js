7abe1a9830f0abdf1d3cf7baa403c12214d9c268
minjaeleee <dlalswo4602@naver.com>
2023-02-17 16:47:38 +0900

    1. 키보드 'keyup' , 'keydown' 활성화 2. 영어만 가능하게 한글 예외처리 - 에러메세지 - 입력 시 빈 string return

--- src/js/keyboard.js
+++ src/js/keyboard.js
@@ -3,19 +3,53 @@ export class Keyboard {
     this.assignElement()
     this.addEvent()
   }
+
   assignElement() {
-    this.switchEl = document.getElementById('switch');
-    this.fontSelectEl = document.getElementById('font');
+    this.containerEl = document.getElementById('container')
+    this.switchEl = this.containerEl.querySelector('#switch')
+    this.fontSelectEl = this.containerEl.querySelector('#font')
+    this.keyboardEl = this.containerEl.querySelector('#keyboard')
+    this.inputGroupEl = this.containerEl.querySelector('#input-group')
+    this.inputEl = this.inputGroupEl.querySelector('#input')
   }
+
   addEvent() {
-    this.switchEl = document.addEventListener('change', event => {
-      document.documentElement.setAttribute(
-        'theme',
-        event.target.checked ? 'dark-mode' : 'white-mode'
-      )
-    })
-    this.fontSelectEl = document.addEventListener('change', event => {
-      document.body.style.fontFamily = event.target.value
-    })
+    this.switchEl = document.addEventListener('change', this.onChangeTheme)
+    this.fontSelectEl = document.addEventListener('change', this.onChangeFont)
+    document.addEventListener('keydown', this.onKeyDown.bind(this))
+    document.addEventListener('keyup', this.onKeyUp.bind(this))
+    this.inputEl.addEventListener('input', this.onInput)
+  }
+
+  onInput(event) {
+    event.target.value = event.target.value.replace(/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/, "")
+  }
+
+  onKeyDown(event) {
+    // 두 번째 인자가 true이면 첫 번째 인자 class 추가
+    this.inputGroupEl.classList.toggle('error',
+      /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/.test(event.key)
+    )
+    this.keyboardEl
+      .querySelector(`[data-code=${event.code}]`)
+      ?.classList.add('active')
+  }
+
+  onKeyUp(event) {
+    this.keyboardEl
+      .querySelector(`[data-code=${event.code}]`)
+      ?.classList.remove('active')
+  }
+
+  onChangeTheme(event) {
+    document.documentElement.setAttribute(
+      'theme',
+      event.target.checked ? 'dark-mode' : 'white-mode'
+    )
+  }
+
+  onChangeFont(event) {
+    document.body.style.fontFamily = event.target.value
+
   }
 }
